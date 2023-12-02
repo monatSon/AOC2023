7dd103adc139600496cd5e84dd4c9311f4fb01d5
Ngoc Son Nguyen <sony.nguyen@externity.cz>
2023-12-02 13:40:40 +0100

    updates 2

--- Day1/AOC2023_Day1/Program.cs
+++ Day1/AOC2023_Day1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = File.ReadAllLines("C:\\Users\\Monat\\Desktop\\Web\\Personal\\AOC2023\\Day1\\AOC2023_Day1\\input\\input.txt");
+
+        foreach (string line in input)
+        {
+            Console.WriteLine(line);
+        }
+
+        Console.ReadLine();
+    }
+
+
+}
+
