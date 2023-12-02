bc8241666689f41ba6a9c886f185af4f3c4386c2
monat_son <sonynakayama@gmail.com>
2023-12-02 15:18:25 +0100

    Add files via upload

---
+++ Day1/AOC23_Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] input = File.ReadAllLines(@"input/input.txt");
+        int sum = 0;
+
+        foreach (string line in input)
+        {
+                var numericDigits = line.Where(char.IsDigit).Select(digit => int.Parse(digit.ToString()));
+                if (numericDigits.Any())
+                {
+                    int calibrationValue = numericDigits.First() * 10 + numericDigits.Last();
+                    sum += calibrationValue;
+                    Console.WriteLine($"Line: {line}, Numeric Digits: {string.Join(", ", numericDigits)}, Calibration Value: {calibrationValue}");
+                }
+        }
+        Console.WriteLine($"Sum of Calibration Values: {sum}");
+        Console.ReadLine();
+    }
+}
